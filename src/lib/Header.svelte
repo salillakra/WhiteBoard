ef7d6b6e6cb3a6af411790911c8e1eecba1214e9
salillakra <salillakra@outlook.com>
2023-12-19 08:23:10 +0530

    added icons,fontawesome,tailwindcss and google fonts

---
+++ src/lib/Header.svelte
@@ -0,0 +1,63 @@
+<h1 class="h-16 text-2xl grid place-items-center bg-slate-800 text-white">
+  Edit your Blogs here
+</h1>
+
+<div class="bg-gray-400 flex items-center p-4 gap-4 flex-wrap">
+  <button
+    class="fa-solid fa-code p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+
+  <select id="font-selector" class="border border-gray-300 rounded px-4 py-2">
+    <option value="">Select a font</option>
+    <option value="Arial">Arial</option>
+    <option value="Verdana">Verdana</option>
+    <option value="Helvetica">Helvetica</option>
+    <option value="Times New Roman">Times New Roman</option>
+    <!-- Add more font options here -->
+  </select>
+
+  <button
+    class="fa-solid fa-bold p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-italic p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-underline p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-image p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-link p-2 bg-slate-50 focus:bg-black active:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+
+  <button
+    class="fa-solid fa-align-left p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-align-center p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-align-right p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-video p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-brands fa-youtube p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+  <button
+    class="fa-solid fa-list p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+
+  <button
+    class="fa-solid fa-list-ol p-2 bg-slate-50 focus:bg-black focus:text-slate-50 rounded-md"
+  ></button>
+</div>
+
+<style>
+  .fa-solid {
+    transition: all 0.2s linear;
+  }
+</style>
